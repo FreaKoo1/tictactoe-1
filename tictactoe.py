8d3d2d9f0fb74711331b9220e63af5f3d463688d
FreaKoo1 <weigangkoo@gmail.com>
2024-05-27 16:49:37 +0800

    adjust board size

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,10 @@ Player2 = "Y"
 winner = None
 response = None
 
+def printboard(board):
+    print(board[0]," | ",board[1]," | ",board[2])
+    print("-------------")
+    print(board[3]," | ",board[4]," | ",board[5])
+    print("-------------")
+    print(board[6]," | ",board[7]," | ",board[8])
+printboard(board)
