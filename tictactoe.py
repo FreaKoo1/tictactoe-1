78e78593288bfc4270a7687e0fa643bc82f86c4f
FreaKoo1 <weigangkoo@gmail.com>
2024-05-27 17:23:24 +0800

    creat playermove function

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,10 @@
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
-Player1 = "X"
-Player2 = "Y"
+player = "X"
 winner = None
 response = None
+freespace = True
 
 def printboard(board):
     print(board[0]," | ",board[1]," | ",board[2])
@@ -12,4 +12,18 @@ def printboard(board):
     print(board[3]," | ",board[4]," | ",board[5])
     print("-------------")
     print(board[6]," | ",board[7]," | ",board[8])
-printboard(board)
+
+
+def player_move(board):
+    while True:
+      inp = int(input("Please select a number[1-9]: "))
+      if inp > 0 and inp < 10 and board[inp -1] == "-":
+        board[inp - 1] = player
+        break
+            
+      else:
+        print("Invalid movement")
+        
+while(winner == None and freespace ):
+    printboard(board)
+    player_move(board)
